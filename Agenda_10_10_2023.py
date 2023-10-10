9fc71f9c165bf7359cf6b175cd40bdbbf8970960
Palomaaihara <palomaaihara@gmail.com>
2023-10-10 20:40:14 -0300

    menu
    menu

--- Agenda_10_10_2023.py
+++ Agenda_10_10_2023.py
@@ -20,6 +20,7 @@ def imprimir_usuario(usuario):
         print("\n")
 
 
+
 #deletar
 def deletar_usuario(usuarios,indice):
     if indice >= 0 and indice < len(usuarios):
@@ -28,6 +29,7 @@ def deletar_usuario(usuarios,indice):
         print("*************************************")
 
         
+
 #atualizar_usuario
 def atualizar_usuario(usuarios, indice, nome, email, telefone):
      if indice >= 0 and indice < len(usuarios):
@@ -37,3 +39,38 @@ def atualizar_usuario(usuarios, indice, nome, email, telefone):
         print("Usuario atualizado com sucesso!")
         print("**********************************")
 
+usuarios = []
+
+while True:
+    print("\n Menu")
+    print("1 - Cadastrar Usuario")
+    print("2 - Imprimir Usuario")
+    print("3 - Atualizar Usuario")
+    print("4 - Deletar Usuario")
+    print("5 - Sair")
+
+    opcao = input("Escolha a opcao desejada: ")
+
+    if opcao == '1':
+        nome = input("Digite o nome do usuario: ")
+        telefone = int(input("Digite o telefone do usuario: "))
+        email = input("Digite o email do usuario: ")
+        cadastra_usuario(usuarios,nome,telefone,email)
+    elif opcao == '2':
+        print(usuarios)
+    elif opcao == '3':
+        indice = int(input("Digite o ID do usuario: "))
+        if 0 <= indice - 1 < len(usuarios):
+            nome = input("Digite o nome do usuario: ")
+            telefone = int(input("Digite o telefone do usuario: "))
+            mail = input("Digite o email do usuario: ")
+            atualizar_usuario(usuarios, indice, nome, email, telefone)
+    elif opcao == '4':
+        indice = int(input("Digite o ID do usuario para deletar: "))
+        if 0 <= indice - 1 < len(usuarios):
+            deletar_usuario(usuarios, indice - 1)
+
+    elif opcao == '5':
+        break
+    else:
+        print("Opcao Invalida!")
