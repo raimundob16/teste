056871d40a7f0394baa1a82fac35fc2429f7619f
raimundob16 <53773141+raimundob16@users.noreply.github.com>
2023-10-16 19:50:34 -0300

    inicio

--- Tarefa_grupo.py
+++ Tarefa_grupo.py
@@ -1,1 +1,7 @@
-print("olar mundo")
+with open("advertising.csv", "r") as arquivo:
+    arquivo_csv = csv.reader(arquivo, delimiter=",")
+    for i, linha in enumerate(arquivo_csv):
+        if i == 0:
+            print("Cabeçalho: " + str(linha))
+        else:
+            print("Valor: " + str(linha))
